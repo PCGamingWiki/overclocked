77d4866383ca83f9f63fe84ea1918b7ad9b2f5c8
Stanisław Gackowski <soeb@soeb.eu>
2017-05-03 12:00:57 +0200

    Google tag

--- Overclocked.php
+++ Overclocked.php
@@ -110,10 +110,10 @@ $GLOBALS['wgSkinOverclockedAds'] = array(
 new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
 \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,\'script\',\'dataLayer\',\'GTM-NS78ZTB\');</script>
+})(window,document,\'script\',\'dataLayer\',\'GTM-PJRR5BG\');</script>
 <!-- End Google Tag Manager -->
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-NS78ZTB"
+<noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-PJRR5BG"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->',
 	'header'  => '<div id="nn_bb1"></div>',
