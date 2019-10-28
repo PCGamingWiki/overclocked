2d95a962f88304b586f4ad5c5dbb66644ed09be7
Andytizer <andytizer@gmail.com>
2019-10-28 17:23:59 +0000

    Revert to NitroPay

--- Overclocked.php
+++ Overclocked.php
@@ -105,39 +105,61 @@ $GLOBALS['wgResourceModuleSkinStyles']['overclocked'] = array(
  * infobox - intended for a square ad above infobox
  */
 $GLOBALS['wgSkinOverclockedAds'] = array(
-	'tag'     => '
-<!-- NetworkN CMP Tool -->
-<script>!function(t,n){if(function(e){"string"==typeof e&&(e=[e]);for(var a=0;a<e.length;a++)n.cookie=e[a]+
-"=;max-age=-1;domain=."+n.domain+";path=/"}(["euconsent","eupubconsent","googlepersonalization"]),
-!t.__cmp){t.__cmp=function(){(t.attachEvent||t.addEventListener)("message",function(e){t.__cmp.receiveMessage(e)}
-,!1),function e(){if(!t.frames.__cmpLocator)if(n.body){var a=n.createElement("iframe");a.style.display="none",
-a.name="__cmpLocator",n.body.appendChild(a)}else setTimeout(e,5)}();var o=[],e=function(e,a,n){"ping"===e?n&&n({
-gdprAppliesGlobally:!!(t.__cmp&&t.__cmp.config&&t.__cmp.config.storeConsentGlobally),cmpLoaded:!1}):
-o.push({command:e,parameter:a,callback:n})};return e.commandQueue=o,e.receiveMessage=function(e){
-var a=e&&e.data&&e.data.__cmpCall;a&&o.push({callId:a.callId,command:a.command,parameter:a.parameter,
-event:e})},e.config=t.cmpConfig||{},e}()}}(window,document);
-</script>
-<script src="//cmp.network-n.com/js/cmp.complete.bundle.js" defer></script>
-
-<!-- NetworkN Ad script -->
-<script src="https://static.pcgamingwiki.com/scripts/network-n-wiki.min.js" async></script>
-
-<script>
-!function(o,n){if(o._localCS={consent:null,loaded:!1,tx:null},o._comscore=o._comscore||[],o.__cmp){
-o.__cmp("getVendorConsents",[77],function(o,c){function e(o){return"object"==typeof o}c&&(_localCS.consent=e(o)
-&&e(o.vendorConsents)&&o.vendorConsents[77],_localCS.consent=_localCS.consent?"1":"0",t())}),function c(){
-o.__cmp("ping",null,function(o){if(!1===o.cmpLoaded){if(null===_localCS.tx)return void(_localCS.tx=setTimeout(c,3e3));
-t()}o.cmpLoaded&&(clearTimeout(_localCS.tx),t())})}()}function t(){var o,c,e;_localCS.loaded||
-(_comscore.push({c1:"2",c2:"25110922",cs_ucfr:_localCS.consent}),c=(o=n).createElement("script"),
-e=o.getElementsByTagName("script")[0],c.async=!0,c.src=("https:"==o.location.protocol?"https://sb":"http://b")
-+".scorecardresearch.com/beacon.js",e.parentNode.insertBefore(c,e),_localCS.loaded=!0)}o.__cmp||t()}(window,document);
-</script>
-<noscript><img src="https://sb.scorecardresearch.com/p?c1=2&c2=25110922&cv=2.0&cj=1" /></noscript>
-<!-- End ComScore Tag -->
-</script>
-',
-	'header'  => '<div id="nn_lb1"></div>',
-	'infobox' => '<div id="nn_mpu1"></div><div id="nn_mobile_mpu1"></div>',
-	'sidebar' => '<div id="nn_sky1"></div>',
-	'footer'  => '<div id="celtra-reveal-wrapper" style="position:fixed; height: auto; width: 100%"><div id="nn_1by1"></div></div><div id="nn_mobile_lb1_sticky"></div><div id="nn_mobile_mpu2"></div>',
+	'tag'     => '<script src="https://s.nitropay.com/ads-51.js"></script>',
+	'header'  => '<div id="nitro-header"></div>
+<script type="text/javascript">
+nads.createAd(\'nitro-header\', {
+  "floor": 0.05,
+  "refreshLimit": 10,
+  "refreshTime": 90,
+  "adsenseSlot": "4173069078",
+  "report": {
+    "enabled": true,
+    "wording": "Report Ad",
+    "position": "fixed-bottom-right"
+  }
+});
+</script>',
+	'sidebar' => '<div id="nitro-sidebar"></div>
+<script type="text/javascript">
+nads.createAd(\'nitro-sidebar\', {
+  "floor": 0.05,
+  "refreshLimit": 10,
+  "refreshTime": 90,
+  "adsenseSlot": "3696108217",
+  "report": {
+    "enabled": true,
+    "wording": "Report Ad",
+    "position": "fixed-bottom-right"
+  }
+});
+</script>',
+	'footer'  => '<div id="nitro-footer"></div>
+<script type="text/javascript">
+nads.createAd(\'nitro-footer\', {
+  "floor": 0.05,
+  "refreshLimit": 10,
+  "refreshTime": 90,
+  "adsenseSlot": "7126535474",
+  "report": {
+    "enabled": true,
+    "wording": "Report Ad",
+    "position": "fixed-bottom-right"
+  }
+});
+</script>',
+	'infobox' => '<div id="nitro-infobox"></div>
+<script type="text/javascript">
+nads.createAd(\'nitro-infobox\', {
+  "floor": 0.05,
+  "refreshLimit": 10,
+  "refreshTime": 90,
+  "adsenseSlot": "5649802273",
+  "report": {
+    "enabled": true,
+    "wording": "Report Ad",
+    "position": "fixed-bottom-right"
+  }
+});
+</script>',
 );
