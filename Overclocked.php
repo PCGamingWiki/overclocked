e3b12a0c879e883036433dfc7cf4365cfb5e2884
Andytizer <andytizer@gmail.com>
2020-06-16 12:46:04 +0100

    Google Tag Manager replacing Nitropay tag

--- Overclocked.php
+++ Overclocked.php
@@ -105,7 +105,13 @@ $GLOBALS['wgResourceModuleSkinStyles']['overclocked'] = array(
  * infobox - intended for a square ad above infobox
  */
 $GLOBALS['wgSkinOverclockedAds'] = array(
-	'tag'     => '<script src="https://s.nitropay.com/ads-51.js"></script>',
+	'tag'     => '<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M4F5J99');</script>
+	<!-- End Google Tag Manager -->',
 	'header'  => '<div id="nitro-header"></div>
 <script type="text/javascript">
 nads.createAd(\'nitro-header\', {
