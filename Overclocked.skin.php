c30429142c959fc30f9b229d5615b36f36f5721a
Andytizer <andytizer@gmail.com>
2020-09-01 17:47:40 +0100

    Lookash changes

--- Overclocked.skin.php
+++ Overclocked.skin.php
@@ -250,7 +250,40 @@ class OverclockedTemplate extends BaseTemplate {
 						?>
 					</ul>
 				</div>
-			</div>
+            </div>
+            <ul id="p-personal-mobile">
+				<?php
+				if( $loggedIn == false ) {
+					foreach ( $personalLogin as $key => $item ) {
+						echo $this->makeListItem( $key, $item );
+					}
+				}
+				else {
+					?>
+					<div id="p-personal-logged-in">
+						<?php
+						foreach ( $personalBar as $key => $item ) {
+							echo $this->makeListItem( $key, $item );
+						}
+						?>
+						
+						<div id="personal-bar-flyout">
+							<div>
+								<a href="<?php echo $personalTools['userpage']['links'][0]['href']; ?>"><?php echo $personalTools['userpage']['links'][0]['text']; ?></a>
+								<ul>
+									<?php
+									foreach ( $personalFlyout as $key => $item ) {
+										echo $this->makeListItem( $key, $item );
+									}
+									?>
+								</ul>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				?>
+			</ul>
 		</div>
 	</header>
 
