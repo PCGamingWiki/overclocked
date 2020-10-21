d9e3b671afcfa4e4bb06ae6691b8cab9669dd264
Idearum <Idearum@users.noreply.github.com>
2020-10-22 00:03:04 +0200

    Added notifications to flyout

--- Overclocked.skin.php
+++ Overclocked.skin.php
@@ -102,29 +102,28 @@ class OverclockedTemplate extends BaseTemplate {
 		}
 		else {
 			$loggedIn = true;
-			$personalBar[0] = $personalTools['watchlist'];
-			$personalBar[1] = $personalTools['mytalk'];
 			$personalBar[2] = $personalTools['notifications-alert'];
 			$personalBar[3] = $personalTools['notifications-notice'];
 
 			$personalFlyout[0] = $personalTools['watchlist'];
-			$personalFlyout[1] = $personalTools['mytalk'];
-			$personalFlyout[2] = $personalTools['mycontris'];
-			$personalFlyout[3] = $personalTools['preferences'];
-			$personalFlyout[5] = $personalTools['logout'];
+			$personalFlyout[1] = $personalTools['notifications'];
+			$personalFlyout[2] = $personalTools['mytalk'];
+			$personalFlyout[3] = $personalTools['mycontris'];
+			$personalFlyout[4] = $personalTools['preferences'];
+			$personalFlyout[6] = $personalTools['logout'];
 			
 			foreach ( $personalFlyout as $key => $item ) {
 				$personalFlyout[$key]['class'] = "group-start";
 			}
 			
 			if ( isset( $personalTools['adminlinks'] ) ) {
-				$personalFlyout[4] = $personalTools['adminlinks'];
+				$personalFlyout[5] = $personalTools['adminlinks'];
 			}
 			foreach ( $personalFlyout as $key => $item ) {
 				$personalFlyout[$key]['id'] = rtrim( $personalFlyout[$key]['id'] . '-flyout' );
 			}
 			/* Work around for Echo preferences. */
-			$personalFlyout[3]['id'] = 'pt-preferences';
+			$personalFlyout[4]['id'] = 'pt-preferences';
 
 			ksort( $personalFlyout );
 
